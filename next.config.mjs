544c3049c66893fea595f110d42e7f62f7a1ba3a
smcoderr <souravoz2018@gmail.com>
2025-03-21 22:43:39 +0530

    all cleared

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'], // Add Unsplash to allowed domains
-      },
-};
-
-export default nextConfig;
