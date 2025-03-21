3906f5dd51fc94c3dc19cd2889d66574cd2ec71c
smcoderr <souravoz2018@gmail.com>
2025-03-21 19:20:55 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com'], // Add Unsplash to allowed domains
+      },
+};
 
 export default nextConfig;
